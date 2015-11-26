175a5229d7afc6f3ee2293190e2fa2e0e7aaef1e
tp-peacock <thomas.peacock@simplybusiness.co.uk>
2015-11-26 16:31:46 +0000

    add validation for passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
 	  validates :name, presence: true, length: { maximum: 30 }
 	  validates :email, presence: true, length: { maximum: 60 }, uniqueness: { case_sensitive: false }
 	  has_secure_password
+	  validates :password, presence: true, length: { minimum: 6 }
 end
