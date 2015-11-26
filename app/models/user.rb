962ca4311bfa6ce56618cc03efb49d2130aa3739
tp-peacock <thomas.peacock@simplybusiness.co.uk>
2015-11-26 16:25:14 +0000

    add has_secure_password for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 	  before_save { self.email = email.downcase }
 	  validates :name, presence: true, length: { maximum: 30 }
 	  validates :email, presence: true, length: { maximum: 60 }, uniqueness: { case_sensitive: false }
-
+	  has_secure_password
 end
