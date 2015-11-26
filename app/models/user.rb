b4377592b527ba01eb94bfe6f001392b31d10d2d
tp-peacock <thomas.peacock@simplybusiness.co.uk>
2015-11-26 16:20:40 +0000

    add uniqueness to user model email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 
 	  before_save { self.email = email.downcase }
 	  validates :name, presence: true, length: { maximum: 30 }
-	  validates :email, presence: true, length: { maximum: 60 }
+	  validates :email, presence: true, length: { maximum: 60 }, uniqueness: { case_sensitive: false }
 
 end
