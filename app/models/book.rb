1ed76e5833f1ff593ca1c8a7607585bde81855cb
tp-peacock <thomas.peacock@simplybusiness.co.uk>
2016-03-07 17:17:18 +0000

    lots of refactoring :)

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,4 +19,25 @@ class Book < ActiveRecord::Base
 		end	
 	end
 
+	def self.search(user, params)
+    books = user.books.where('author LIKE ?', '%' + params + '%')
+    books += user.books.where('title LIKE ?', '%' + params + '%')
+	  books.uniq
+	end
+
+	def self.filter(user, params)
+    params = params - ['']
+
+    if params.empty?
+      user.books.select do |book|        
+        !book.physical && !book.ebook && !book.read
+      end
+    else
+      user.books.select do |book|
+      	(!params.include?("physical") || book.physical) &&
+				(!params.include?("ebook")    || book.ebook) && 
+				(!params.include?("read")     || book.read) 
+      end
+    end
+	end
 end
