1ead677f9a4df506aaa9a27203adfa383382a8eb
tp-peacock <thomas.peacock@simplybusiness.co.uk>
2016-05-06 14:54:19 +0100

    add number of pages to book view, new book and edit book screens

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,7 +64,7 @@ class BooksController < ApplicationController
   private
     
     def book_params
-      params.require(:book).permit(:title, :processed_title, :author, :physical, :ebook, :read)
+      params.require(:book).permit(:title, :processed_title, :author, :pages, :start_date, :end_date, :physical, :ebook, :read)
     end
 
     def ordering
